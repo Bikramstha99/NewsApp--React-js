510aedac1c7afa69930149c69c26b2881c39c109
Bikram Shrestha <stha.bikram999@gmail.com>
2024-03-13 23:45:06 +0545

    adding some html and css in the new news and newsItem component

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem';
 
 export class News extends Component {
   render() {
+    
     return (
       <div>
-        This is a new component
+        <div className='container'>
+            <h2>Top news headlines</h2>
+            <div className="row">
+                <div  className='col-md-4'>
+                <NewsItem title='bikram' description='don'></NewsItem>
+                </div>
+                <div  className='col-md-4'>
+                <NewsItem title='bikram' description='don'></NewsItem>
+                </div>
+                <div  className='col-md-4'>
+                <NewsItem title='bikram' description='don'></NewsItem>
+                </div>
+            </div>
       </div>
+     </div>
     )
   }
 }
